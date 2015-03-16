6261e89e20c31fdabda778ff5f77cc1345124cb7
Charles Darwin Pobre <cpobre.peg@gmail.com>
2015-03-16 16:36:23 +0800

    resources

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ var App = Ember.Application.create({
 
 App.Router.map(function(){
   this.route('about');
+  this.resource('products');
+  this.resource('product', { path: '/products/:title' });
 });
+
+App.PRODUCTS = [
+  {
+    title: 'Apple',
+    price: 99,
+    description: 'Apple is Love',
+    isOnSale: true,
+    image: 'images/apple.jpeg'
+  },
+  {
+    title: 'Gear',
+    price: 75,
+    description: 'Gears',
+    isOnSale: true,
+    image: 'images/gear.jpeg'
+  },
+  {
+    title: 'Shoe',
+    price: 125,
+    description: 'Shoe',
+    isOnSale: true,
+    image: 'images/shoe.jpeg'
+  }
+]
 
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
@@ -12,4 +38,16 @@ App.IndexController = Ember.Controller.extend({
   time: function(){
     return(new Date()).toDateString()
   }.property()
+});
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function(){
+    return App.PRODUCTS;
+  }
+});
+
+App.ProductRoute = Ember.Route.extend({
+  model: function(params){
+    return App.PRODUCTS.findBy('title', params.title);
+  }
 });
