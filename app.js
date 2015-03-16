d96950437a4bc6db59db367fb40f544577207c6b
Charles Darwin Pobre <cpobre.peg@gmail.com>
2015-03-16 19:58:58 +0800

    fixed outdated syntax on model

--- app.js
+++ app.js
@@ -2,36 +2,53 @@ var App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
+App.Store = DS.Store.extend({
+  adapter: DS.FixtureAdapter.create()
+})
 App.Router.map(function(){
   this.route('about');
   this.resource('products', function(){
-    this.resource('product', { path: '/:title' });
+    this.resource('product', { path: '/:product_id' });
   });
 });
 
-App.PRODUCTS = [
-  {
-    title: 'Apple',
-    price: 99,
-    description: 'Apple is Love',
-    isOnSale: true,
-    image: 'images/apple.jpeg'
-  },
-  {
-    title: 'Gear',
-    price: 75,
-    description: 'Gears',
-    isOnSale: true,
-    image: 'images/gear.jpeg'
-  },
-  {
-    title: 'Shoe',
-    price: 125,
-    description: 'Shoe',
-    isOnSale: true,
-    image: 'images/shoe.jpeg'
-  }
-]
+App.Product = DS.Model.extend({
+  title: DS.attr('string'),
+  price: DS.attr('number'),
+  description: DS.attr('string'),
+  isOnSale: DS.attr('number'),
+  image: DS.attr('string')
+});
+
+App.Product.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      title: 'Apple',
+      price: 99,
+      description: 'Apple is Love',
+      isOnSale: true,
+      image: 'images/apple.jpeg'
+    },
+    {
+      id: 2,
+      title: 'Gear',
+      price: 75,
+      description: 'Gears',
+      isOnSale: true,
+      image: 'images/gear.jpeg'
+    },
+    {
+      id: 3,
+      title: 'Shoe',
+      price: 125,
+      description: 'Shoe',
+      isOnSale: true,
+      image: 'images/shoe.jpeg'
+    }
+  ]
+
+})
 
 App.IndexController = Ember.Controller.extend({
   productsCount: 6,
@@ -43,12 +60,12 @@ App.IndexController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
   model: function(){
-    return App.PRODUCTS;
+    return this.store.find('product');
   }
 });
 
 App.ProductRoute = Ember.Route.extend({
   model: function(params){
-    return App.PRODUCTS.findBy('title', params.title);
+    return this.store.find('product', params.product_id);
   }
 });
